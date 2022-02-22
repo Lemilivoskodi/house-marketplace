c00e07003e8469958d99237cdcb01dbe91d9ee43
Makazoruki <lavita.jagoda@gmail.com>
2022-02-22 23:13:39 +0100

    edit listings+more

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-
+import { useParams } from 'react-router-dom'
 import {
   collection,
   getDocs,
@@ -14,10 +14,12 @@ import { toast } from 'react-toastify'
 import Spinner from '../components/Spinner'
 import ListingItem from '../components/ListingItem'
 
-function Offers() {
+function Category() {
   const [listings, setListings] = useState(null)
   const [loading, setLoading] = useState(true)
   const [lastFetchedListing, setLastFetchedListing] = useState(null)
+
+  const params = useParams()
 
   useEffect(() => {
     const fetchListings = async () => {
@@ -28,7 +30,7 @@ function Offers() {
         // Create a query
         const q = query(
           listingsRef,
-          where('offer', '==', true),
+          where('type', '==', params.categoryName),
           orderBy('timestamp', 'desc'),
           limit(10)
         )
@@ -54,8 +56,7 @@ function Offers() {
     }
 
     fetchListings()
-  }, [])
-
+  }, [params.categoryName])
   // Pagination // Load Moire
   const onFetchMoreLisitngs = async () => {
     try {
@@ -65,7 +66,7 @@ function Offers() {
       // Create a query
       const q = query(
         listingsRef,
-        where('offer', '==', true),
+        where('type', '==', params.categoryName),
         orderBy('timestamp', 'desc'),
         startAfter(lastFetchedListing),
         limit(10)
@@ -90,10 +91,15 @@ function Offers() {
       toast.error('Could not fetch listings')
     }
   }
+
   return (
     <div className='category'>
       <header>
-        <p className='pageHeader'>Offers</p>
+        <p className='pageHeader'>
+          {params.categoryName === 'rent'
+            ? 'Places for rent'
+            : 'Places for sale'}
+        </p>
       </header>
 
       {loading ? (
@@ -111,6 +117,7 @@ function Offers() {
               ))}
             </ul>
           </main>
+
           <br />
           <br />
           {lastFetchedListing && (
@@ -120,10 +127,10 @@ function Offers() {
           )}
         </>
       ) : (
-        <p> There are no current offers </p>
+        <p>No listings for {params.categoryName} </p>
       )}
     </div>
   )
 }
 
-export default Offers
+export default Category
